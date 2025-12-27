bc787ac80e2b278f02f3ae22e0f2d369df03d732
snowcat13 <121311428+sooinji@users.noreply.github.com>
2025-12-27 18:48:57 +0900

    chore: 개발 환경 설정 (#1)

---
+++ src/test/java/com/promlog/promlog/PromlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promlog.promlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
