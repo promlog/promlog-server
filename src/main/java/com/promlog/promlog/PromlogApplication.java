ededb460e1877d6c7939e7fb86122509b4fc3d4c
수인 <121311428+sooinji@users.noreply.github.com>
2025-12-28 17:34:19 +0900

    chore: 개발 환경 설정 (#1)

---
+++ src/main/java/com/promlog/promlog/PromlogApplication.java
@@ -0,0 +1,13 @@
+package com.promlog.promlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromlogApplication.class, args);
+	}
+
+}
